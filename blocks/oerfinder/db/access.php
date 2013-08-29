65603f8144ccad597903fe045b60cf00a12c3d5a
tafkey <tafkey@about.me>
2013-08-30 02:03:26 +0300

    Fixes #3, #4 and #5

--- blocks/oerfinder/db/access.php
+++ blocks/oerfinder/db/access.php
@@ -4,15 +4,25 @@
  *
  * @package   block_oerfinder
  * @developer Tasos Koutoumanos <anastasios.koutoumanos@gmail.com>
- * @copyright 2012 AgroKnow Technologies
+ * @copyright 2012, 2013 AgroKnow Technologies
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
+    
+    // 'block/oerfinder:myaddinstance' => array(
+    //     'captype' => 'write',
+    //     'contextlevel' => CONTEXT_SYSTEM,
+    //     'archetypes' => array(
+    //         'user' => CAP_ALLOW
+    //     ),
 
-    'block/blog_oerfinder:addinstance' => array(
+    //     'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    // ),
+
+    'block/oerfinder:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
         'captype' => 'write',
--- blocks/oerfinder/version.php
+++ blocks/oerfinder/version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2012122701;
+$plugin->version = 2013083001;
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 
 $plugin->component = 'block_oerfinder'; // Full name of the plugin (used for diagnostics)
