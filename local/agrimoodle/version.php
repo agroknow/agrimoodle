7d3d2b2dac1987993282a7e9e22d2a72fdc0a561
tafkey <tafkey@about.me>
2013-10-17 18:53:00 +0300

    Working on agrimoodle local plugin for automatic registration with the hub.

--- local/agrimoodle/version.php
+++ local/agrimoodle/version.php
@@ -1,6 +1,11 @@
 <?php
 
-$plugin->version = 2012061801;
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 
+$plugin->version = 2013101722;
+$plugin->release = '1.0 (Build: ' . $plugin->version . ')';
+
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_agrimoodle'; // Full name of the plugin (used for diagnostics)
+
+
