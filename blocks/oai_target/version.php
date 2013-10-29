4ed6eae922ad056b209b1492271b62ffb6a0ecbf
gkista <gkista@agroknow.gr>
2013-10-29 14:28:48 +0200

    Fix oai_target version

--- blocks/oai_target/version.php
+++ blocks/oai_target/version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component  = 'block_oai_target';
-$plugin->version  = 2013022002;
+$plugin->version  = 2013102901;
 $plugin->release  = 1;
 $plugin->requires  =2011033003;
 $plugin->maturity  = MATURITY_BETA;
