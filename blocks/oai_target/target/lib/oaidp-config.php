0bef8b4420172a0a0aeb7417be04122fb87fc82d
tafkey <tafkey@about.me>
2013-10-29 23:55:33 +0200

    working on oai-target, almost done!

--- blocks/oai_target/target/lib/oaidp-config.php
+++ blocks/oai_target/target/lib/oaidp-config.php
@@ -1,46 +1,46 @@
 <?php
 
 /*
-* +----------------------------------------------------------------------+
-* | PHP Version 4                                                        |
-* +----------------------------------------------------------------------+
-* | Copyright (c) 2002-2005 Heinrich Stamerjohanns                       |
-* |                                                                      |
-* | oaidp-config.php -- Configuration of the OAI Data Provider           |
-* |                                                                      |
-* | This is free software; you can redistribute it and/or modify it under|
-* | the terms of the GNU General Public License as published by the      |
-* | Free Software Foundation; either version 2 of the License, or (at    |
-* | your option) any later version.                                      |
-* | This software is distributed in the hope that it will be useful, but |
-* | WITHOUT  ANY WARRANTY; without even the implied warranty of          |
-* | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
-* | GNU General Public License for more details.                         |
-* | You should have received a copy of the GNU General Public License    |
-* | along with  software; if not, write to the Free Software Foundation, |
-* | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA         |
-* |                                                                      |
-* +----------------------------------------------------------------------+
-* | Derived from work by U. Mόller, HUB Berlin, 2002                     |
-* |                                                                      |
-* | Written by Heinrich Stamerjohanns, May 2002                          |
-* |            stamer@uni-oldenburg.de                                   |
-* +----------------------------------------------------------------------+
-*/
+ * +----------------------------------------------------------------------+
+ * | PHP Version 4                                                        |
+ * +----------------------------------------------------------------------+
+ * | Copyright (c) 2002-2005 Heinrich Stamerjohanns                       |
+ * |                                                                      |
+ * | oaidp-config.php -- Configuration of the OAI Data Provider           |
+ * |                                                                      |
+ * | This is free software; you can redistribute it and/or modify it under|
+ * | the terms of the GNU General Public License as published by the      |
+ * | Free Software Foundation; either version 2 of the License, or (at    |
+ * | your option) any later version.                                      |
+ * | This software is distributed in the hope that it will be useful, but |
+ * | WITHOUT  ANY WARRANTY; without even the implied warranty of          |
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
+ * | GNU General Public License for more details.                         |
+ * | You should have received a copy of the GNU General Public License    |
+ * | along with  software; if not, write to the Free Software Foundation, |
+ * | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA         |
+ * |                                                                      |
+ * +----------------------------------------------------------------------+
+ * | Derived from work by U. Mόller, HUB Berlin, 2002                     |
+ * |                                                                      |
+ * | Written by Heinrich Stamerjohanns, May 2002                          |
+ * |            stamer@uni-oldenburg.de                                   |
+ * +----------------------------------------------------------------------+
+ */
 //
 // $Id: oaidp-config.php,v 1.07 2004/07/01 16:59:57 stamer Exp $
 //
 
-/* 
+/*
  * This is the configuration file for the PHP OAI Data-Provider.
  * Please read through the WHOLE file, there are several things, that 
  * need to be adjusted:
 
- - where to find the PEAR classes (look for PEAR SETUP)
- - parameters for your database connection (look for DATABASE SETUP)
- - the name of the table where you store your data
- - the encoding your data is stored (all below DATABASE SETUP)
-*/
+  - where to find the PEAR classes (look for PEAR SETUP)
+  - parameters for your database connection (look for DATABASE SETUP)
+  - the name of the table where you store your data
+  - the encoding your data is stored (all below DATABASE SETUP)
+ */
 
 // To install, test and debug use this
 // If set to TRUE, will die and display query and database error message
@@ -52,7 +52,6 @@ $SHOW_QUERY_ERROR = FALSE;
 // The content-type the WWW-server delivers back. For debug-puposes, "text/plain"
 // is easier to view. On a production site you should use "text/xml".
 //$CONTENT_TYPE = 'Content-Type: text/plain';
-
 // If everything is running ok, you should use this
 // $SHOW_QUERY_ERROR = FALSE;
 $CONTENT_TYPE = 'Content-Type: text/xml';
@@ -65,7 +64,6 @@ $CONTENT_TYPE = 'Content-Type: text/xml';
 // Windows users might like to try this
 //ini_set('include_path', '.;c:\wamp\bin\php\php5.3.5\pear');
 //ini_set('include_path', '.;C:\wamp\bin\php\php5.3.5\pear\pear');
-
 // if there are problems with unknown 'numrows', then make sure
 // to upgrade to a decent PEAR version.
 
@@ -74,16 +72,15 @@ require_once('DB.php');
 error_reporting(E_ALL & ~E_NOTICE);
 
 // do not change
-$MY_URI = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
+$MY_URI = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'];
 
 // MUST (only one)
 // please adjust
-$repositoryName       = 'An agriMoodle\'s instance OAI Repository';
-$baseURL			  = $MY_URI;
-// You can use a static URI as well.
+$repositoryName = 'An agriMoodle\'s instance OAI Repository';
+$baseURL = $MY_URI;
 //$baseURL 			= "http://localhost/oai/oai2.php";
 // do not change
-$protocolVersion      = '2.0';
+$protocolVersion = '2.0';
 
 // How your repository handles deletions
 // no: 			The repository does not maintain status about deletions.
@@ -98,28 +95,28 @@ $protocolVersion      = '2.0';
 // Currently if $record['deleted'] is set to 'true', $status_deleted is set.
 // Some lines in listidentifiers.php, listrecords.php, getrecords.php
 // must be changed to fit the condition for your database.
-$deletedRecord        = 'no';
+$deletedRecord = 'no';
 
 // MAY (only one)
 //granularity is days
 //$granularity          = 'YYYY-MM-DD';
 // granularity is seconds
-$granularity          = 'YYYY-MM-DDThh:mm:ssZ';
+$granularity = 'YYYY-MM-DDThh:mm:ssZ';
 
 // MUST (only one)
 // the earliest datestamp in your repository,
 // please adjust
-$earliestDatestamp    = '2012-01-01';
+$earliestDatestamp = '2012-01-01';
 
 // this is appended if your granularity is seconds.
 // do not change
 if ($granularity == 'YYYY-MM-DDThh:mm:ss:Z') {
-	$earliestDatestamp .= 'T00:00:00Z';
+  $earliestDatestamp .= 'T00:00:00Z';
 }
 
 // MUST (multiple)
 // please adjust
-$adminEmail			= array('mailto:webmaster@agrimoodle.org');
+$adminEmail = array('mailto:webmaster@agrimoodle.org');
 
 // MAY (multiple)
 // Comment out, if you do not want to use it.
@@ -128,10 +125,9 @@ $adminEmail			= array('mailto:webmaster@agrimoodle.org');
 // The client MUST send "Accept-Encoding: gzip" to actually receive
 // compressed output.
 // $compression		= array('gzip');
-
 // MUST (only one)
 // should not be changed
-$delimiter			= ':';
+$delimiter = ':';
 
 // MUST (only one)
 // You may choose any name, but for repositories to comply with the oai
@@ -141,7 +137,7 @@ $delimiter			= ':';
 // (scheme ":" namespace_id ":" local_id where scheme should always be "oai")
 // $parseURL = parse_url($CFG->wwwroot);
 // $repositoryIdentifier = $parseURL['host'].''.$parseURL['path']; 
-$repositoryIdentifier = parse_url($CFG->wwwroot)['host']; 
+$repositoryIdentifier = parse_url($CFG->wwwroot)['host'];
 
 
 // description is defined in identify.php
@@ -151,7 +147,6 @@ $show_identifier = true;
 // data-providers).
 // Please check identify.php for other possible containers
 // in the Identify response
-
 // maximum mumber of the records to deliver
 // (verb is ListRecords)
 // If there are more records to deliver
@@ -165,23 +160,22 @@ $MAXRECORDS = 50;
 $MAXIDS = 200;
 
 // After 24 hours resumptionTokens become invalid.
-$tokenValid = 24*3600;
-$expirationdatetime = gmstrftime('%Y-%m-%dT%TZ', time()+$tokenValid);
+$tokenValid = 24 * 3600;
+$expirationdatetime = gmstrftime('%Y-%m-%dT%TZ', time() + $tokenValid);
 
 //INDENT for the xmlrecord()
 $INDENT = 4;
 
 // define all supported sets in your repository
-$SETS = 	array (
-				//array("setSpec"=>"phdthesis", 'setName'=>'PHD Thesis', 'setDescription'=>'Very nice thesis'),
-				array("setSpec"=>"courses", 'setName'=>'Courses that are deployed through agriMoodle') ,
-				array("setSpec"=>"resources", 'setName'=>'Resources from agriMoodle courses'),
-	// array('setSpec'=>'math', 'setName'=>'Mathematics') ,
-	// array('setSpec'=>'phys', 'setName'=>'Physics')
+$SETS = array(
+    //array("setSpec"=>"phdthesis", 'setName'=>'PHD Thesis', 'setDescription'=>'Very nice thesis'),
+    array("setSpec" => "courses", 'setName' => 'Courses that are deployed through agriMoodle'),
+    array("setSpec" => "resources", 'setName' => 'Resources from agriMoodle courses'),
+        // array('setSpec'=>'math', 'setName'=>'Mathematics') ,
+        // array('setSpec'=>'phys', 'setName'=>'Physics')
 );
 
 // define all supported metadata formats
-
 //
 // myhandler is the name of the file that handles the request for the
 // specific metadata format.
@@ -189,32 +183,31 @@ $SETS = 	array (
 // [record_namespace] describe the namespace for this prefix
 
 
-$METADATAFORMATS = 	array (
-	'oai_dc' => array('metadataPrefix'=>'oai_dc',
-		'schema'=>'http://www.openarchives.org/OAI/2.0/oai_dc.xsd',
-		'metadataNamespace'=>'http://www.openarchives.org/OAI/2.0/oai_dc/',
-		'myhandler'=>'record_dc.php',
-		'record_prefix'=>'dc',
-		'record_namespace' => 'http://purl.org/dc/elements/1.1/'
-	),
-	'oai_lom' => array('metadataPrefix'=>'oai_lom',
-		'schema'=>'http://www.openarchives.org/OAI/2.0/oai_dc.xsd',
-		'metadataNamespace'=>'http://www.openarchives.org/OAI/2.0/oai_dc/',
-		'myhandler'=>'record_lom.php',
-		'record_prefix'=>'lom',
-		'record_namespace' => 'http://ltsc.ieee.org/xsd/LOM'
-	)
+$METADATAFORMATS = array(
+    'oai_dc' => array('metadataPrefix' => 'oai_dc',
+        'schema' => 'http://www.openarchives.org/OAI/2.0/oai_dc.xsd',
+        'metadataNamespace' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        'myhandler' => 'record_dc.php',
+        'record_prefix' => 'dc',
+        'record_namespace' => 'http://purl.org/dc/elements/1.1/'
+    ),
+    'oai_lom' => array('metadataPrefix' => 'oai_lom',
+        'schema' => 'http://www.openarchives.org/OAI/2.0/oai_dc.xsd',
+        'metadataNamespace' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        'myhandler' => 'record_lom.php',
+        'record_prefix' => 'lom',
+        'record_namespace' => 'http://ltsc.ieee.org/xsd/LOM'
+    )
 );
 
 //
 // DATABASE SETUP
 //
-
 // change according to your local DB setup.
-$DB_HOST   = $CFG->dbhost;
-$DB_USER   = $CFG->dbuser;
+$DB_HOST = $CFG->dbhost;
+$DB_USER = $CFG->dbuser;
 $DB_PASSWD = $CFG->dbpass;
-$DB_NAME   = $CFG->dbname;
+$DB_NAME = $CFG->dbname;
 
 // Data Source Name: This is the universal connection string
 // if you use something other than mysql edit accordingly.
@@ -223,7 +216,6 @@ $DSN = "mysql://$DB_USER:$DB_PASSWD@$DB_HOST/$DB_NAME";
 
 // Example for Oracle
 // $DSN = "oci8://$DB_USER:$DB_PASSWD@$DB_NAME";
-
 // the charset you store your metadata in your database
 // currently only utf-8 and iso8859-1 are supported
 $charset = "utf-8";
@@ -257,10 +249,10 @@ $idPrefix = 'v1/';
 // this will be expanded to
 // oai:$repositoryIdentifier:$idPrefix$SQL['identifier']
 // should not be changed
-$oaiprefix = "oai".$delimiter.$repositoryIdentifier.$delimiter.$idPrefix; 
+$oaiprefix = "oai" . $delimiter . $repositoryIdentifier . $delimiter . $idPrefix;
 
 // adjust anIdentifier with sample contents an identifier
-$sampleIdentifier     = $oaiprefix.'c21/r1972';
+$sampleIdentifier = $oaiprefix . 'c21/r1972';
 
 // the name of the column where you store your datestamps
 $SQL['datestamp'] = 'datestamp';
@@ -278,84 +270,75 @@ $SQL['set'] = 'oai_set';
 // Here are a couple of queries which might need to be adjusted to
 // your needs. Normally, if you have correctly named the columns above,
 // this does not need to be done.
-
 // this function should generate a query which will return
 // all records
 // the useless condition id_column = id_column is just there to ease
 // further extensions to the query, please leave it as it is.
-function selectallQuery ($id = '')
-{
-	global $SQL;
-	$query = 'SELECT * FROM '.$SQL['table'].' WHERE ';
-	if ($id == '') {
-		$query .= $SQL['id_column'].' = '.$SQL['id_column'];
-	}
-	else {
-		$query .= $SQL['identifier']." ='$id'";
-	}
-	return $query;
+function selectallQuery($id = '') {
+  global $SQL;
+  $query = 'SELECT * FROM ' . $SQL['table'] . ' WHERE ';
+  if ($id == '') {
+    $query .= $SQL['id_column'] . ' = ' . $SQL['id_column'];
+  } else {
+    $query .= $SQL['identifier'] . " ='$id'";
+  }
+  return $query;
 }
 
 // this function will return identifier and datestamp for all records
-function idQuery ($id = '')
-{
-	global $SQL;
+function idQuery($id = '') {
+  global $SQL;
 
-	if ($SQL['set'] != '') {
-		$query = 'select '.$SQL['identifier'].','.$SQL['datestamp'].','.$SQL['set'].' FROM '.$SQL['table'].' WHERE ';
-	} else {
-		$query = 'select '.$SQL['identifier'].','.$SQL['datestamp'].' FROM '.$SQL['table'].' WHERE ';
-	}
+  if ($SQL['set'] != '') {
+    $query = 'select ' . $SQL['identifier'] . ',' . $SQL['datestamp'] . ',' . $SQL['set'] . ' FROM ' . $SQL['table'] . ' WHERE ';
+  } else {
+    $query = 'select ' . $SQL['identifier'] . ',' . $SQL['datestamp'] . ' FROM ' . $SQL['table'] . ' WHERE ';
+  }
 
-	if ($id == '') {
-		$query .= $SQL['id_column'].' = '.$SQL['id_column'];
-	}
-	else {
-		$query .= $SQL['identifier']." = '$id'";
-	}
+  if ($id == '') {
+    $query .= $SQL['id_column'] . ' = ' . $SQL['id_column'];
+  } else {
+    $query .= $SQL['identifier'] . " = '$id'";
+  }
 
-	return $query;
+  return $query;
 }
 
 // filter for until
-function untilQuery($until)
-{
-	global $SQL;
+function untilQuery($until) {
+  global $SQL;
 
-	return ' and '.$SQL['datestamp']." <= '$until'";
+  return ' and ' . $SQL['datestamp'] . " <= '$until'";
 }
 
 // filter for from
-function fromQuery($from)
-{
-	global $SQL;
+function fromQuery($from) {
+  global $SQL;
 
-	return ' and '.$SQL['datestamp']." >= '$from'";
+  return ' and ' . $SQL['datestamp'] . " >= '$from'";
 }
 
 // filter for sets
-function setQuery($set)
-{
-	global $SQL;
+function setQuery($set) {
+  global $SQL;
 
-	return ' and '.$SQL['set']." LIKE '%$set%'";
+  return ' and ' . $SQL['set'] . " LIKE '%$set%'";
 }
 
 // There is no need to change anything below.
-
 // Current Date
 $datetime = gmstrftime('%Y-%m-%dT%T');
-$responseDate = $datetime.'Z';
+$responseDate = $datetime . 'Z';
 
 // do not change
 $XMLHEADER = '<?xml version="1.0" encoding="UTF-8"?>
 <OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/"
 	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
-	http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">'."\n";
+	http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd">' . "\n";
 
 $xmlheader = $XMLHEADER .
-	' <responseDate>'.$responseDate."</responseDate>\n";
+        ' <responseDate>' . $responseDate . "</responseDate>\n";
 
 // the xml schema namespace, do not change this
 $XMLSCHEMA = 'http://www.w3.org/2001/XMLSchema-instance';
--- blocks/oai_target/version.php
+++ blocks/oai_target/version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component  = 'block_oai_target';
-$plugin->version  = 2013102901;
+$plugin->version  = 20131029901;
 $plugin->release  = 1;
 $plugin->requires  =2011033003;
 $plugin->maturity  = MATURITY_BETA;
