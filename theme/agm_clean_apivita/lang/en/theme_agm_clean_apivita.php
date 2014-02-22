12f93f1ff279b7d0e01f436817c74eb9d192f1d2
gkista <gkista@agroknow.gr>
2014-02-22 17:51:17 +0200

    New theme for apivita

---
+++ theme/agm_clean_apivita/lang/en/theme_agm_clean_apivita.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_agm_clean', language 'en'
+ *
+ * @package   theme_agm_clean
+ * @copyright 2013 Moodle, moodle.org
+ * @copyright 2013 AgroKnow, agroknow.gr
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>agriMoodle Clean</h2>
+<p><img class=img-polaroid src="agm_clean/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>agriMoodle Clean is based on the <i>Clean theme</i>, which has been redesigned and adopted to meet the needs of <b><a href="http://agrimoodle.org>agriMoodle</a></b>.</p>
+<h3>Parents</h3>
+<p>Clean is a modified Moodle Bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<p>Clean is therefore based upon the Bootstrap theme, which was created for Moodle 2.5, with the help of:<br>
+Stuart Lamour, Mark Aberdour, Paul Hibbitts, Mary Evans.</p>
+<h3>Theme Credits</h3>
+<p>Authors: Bas Brands, David Scotson, Mary Evans<br>
+Contact: bas@sonsbeekmedia.nl<br>
+Website: <a href="http://www.basbrands.nl">www.basbrands.nl</a>
+</p>
+<h3>Report a bug:</h3>
+<p><a href="http://tracker.moodle.org">http://tracker.moodle.org</a></p>
+<h3>More information</h3>
+<p><a href="agm_clean/README.txt">How to copy and customise this theme.</a></p>
+</div></div>';
+
+$string['configtitle'] = 'agriMoodle Apivita';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+If the height of your logo is more than 75px add the following CSS rule to the Custom CSS box below.<br>
+a.logo {height: 100px;} or whatever height in pixels the logo is.';
+
+$string['pluginname'] = 'agriMoodle Apivita';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+
