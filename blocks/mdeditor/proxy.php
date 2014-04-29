18b3758e7a10f7a4130dc41ec9e7b0f217748697
gkista <gkista@agroknow.gr>
2014-04-29 12:24:22 +0300

    [mdeditor] new proxy

--- blocks/mdeditor/proxy.php
+++ blocks/mdeditor/proxy.php
@@ -1,19 +1,18 @@
 <?php
-                   
-$url = $_GET['url'];
-$url =  str_replace(" ","%20", $url);
-//$url = 'http://research.celi.it:8080/DomainTerminologyChecker/rest/domain_terminology_checker?translation=En+consecuencia%2C+los+agricultores+tienden+a+implementar+una+estrategia+de+control+de+malezas+no+enfocado+amplio%2C+sobre+la+base+de+productos+de+amplio+espectro+y+las+mezclas+de+diferentes+productos.&to=es&source=Consequently%2C+farmers+tend+to+implement+a+broad+non+focused+weed+control+strategy%2C+on+the+basis+of+broad+spectrum+products+and+mixtures+of+different+products.&from=en&service=microsoft&domainID=organic.lingua&json_output=true';
 
+$url = $_GET['url'];
+//$url = str_replace(" ", "%20", $url);
+$url = urlencode($url);
+
+//$url = 'http://research.celi.it:8080/DomainTerminologyChecker/rest/domain_terminology_checker?translation=En+consecuencia%2C+los+agricultores+tienden+a+implementar+una+estrategia+de+control+de+malezas+no+enfocado+amplio%2C+sobre+la+base+de+productos+de+amplio+espectro+y+las+mezclas+de+diferentes+productos.&to=es&source=Consequently%2C+farmers+tend+to+implement+a+broad+non+focused+weed+control+strategy%2C+on+the+basis+of+broad+spectrum+products+and+mixtures+of+different+products.&from=en&service=microsoft&domainID=organic.lingua&json_output=true';
 //gets the data from a URL
-function get_url($url)
-{
+function get_url($url) {
     $ch = curl_init();
-      
-    if($ch === false)
-    {
+
+    if ($ch === false) {
         die('Failed to create curl object');
     }
-      
+
     $timeout = 5;
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -22,7 +21,6 @@ function get_url($url)
     curl_close($ch);
     return $data;
 }
-  
-echo get_url($url);
 
+echo get_url($url);
 ?>
