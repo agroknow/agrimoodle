7d6dce428115ef7f0924842d096c48336676e64e
gkista <gkista@agroknow.gr>
2014-04-29 12:45:35 +0300

    [mdeditor] new proxy add filters

--- blocks/mdeditor/proxy.php
+++ blocks/mdeditor/proxy.php
@@ -1,8 +1,11 @@
 <?php
 
 $url = $_GET['url'];
-//$url = str_replace(" ", "%20", $url);
-$url = urlencode($url);
+$url = str_replace(" ", "%20", $url);
+$url = str_replace("/r", "%20", $url);
+$url = str_replace("/n", "%20", $url);
+$url = str_replace("\r", "%20", $url);
+$url = str_replace("\n", "%20", $url);
 
 //$url = 'http://research.celi.it:8080/DomainTerminologyChecker/rest/domain_terminology_checker?translation=En+consecuencia%2C+los+agricultores+tienden+a+implementar+una+estrategia+de+control+de+malezas+no+enfocado+amplio%2C+sobre+la+base+de+productos+de+amplio+espectro+y+las+mezclas+de+diferentes+productos.&to=es&source=Consequently%2C+farmers+tend+to+implement+a+broad+non+focused+weed+control+strategy%2C+on+the+basis+of+broad+spectrum+products+and+mixtures+of+different+products.&from=en&service=microsoft&domainID=organic.lingua&json_output=true';
 //gets the data from a URL
